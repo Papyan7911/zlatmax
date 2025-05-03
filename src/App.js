7eddbe5235f2ea0467c8b877beade1462fc696ff
Samo-Maruqyan <maruqyan55@mail.ru>
2025-05-03 22:27:50 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Catalog from './pages/Catalog/Catalog';
-import Firstsection from './components/FirstSection/FirstSection';
-import Secondsection from './components/Secondsection/Secondsection';
-import  Card  from './components/Card/Card';
+import React from 'react';
+import Footer from './components/footer/footer';
+import Card from './components/card/card';
+
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Firstsection/>
-      <Secondsection/>
-      <Card/>
-      <Routes>
-        <Route path="/catalog" element={<Catalog />} />
-      </Routes>
-    </Router>
+    <div className="container">
+      <Card />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/footer/footerLink.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// const FooterLink = ({ label, href }) => {
+//   return (
+//     <p>
+//       <a href={href} target="_blank" rel="noopener noreferrer">
+//         {label}
+//       </a>
+//     </p>
+//   );
+// };
+
+// export default FooterLink;
